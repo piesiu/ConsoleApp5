8e5f85d83002704a41c04fef0049e7768c2a1477
Piotr Lech <plech@apptrans.pl>
2018-01-15 21:55:43 +0100

    dodana klasa

--- ConsoleApp5/Class1.cs
+++ ConsoleApp5/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello world");
-        }
     }
 }
